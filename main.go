ea509e00e4a4d0644edca7d95750a6605abcc24e
stamp <stampz74@gmail.com>
2018-05-07 17:32:46 +0700

    Added source code and built plugin

---
+++ main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+
+	lib "github.com/masaruz/engine-lib"
+)
+
+type form = struct {
+	Header    string
+	Body      string
+	Signature string
+}
+
+type ack = func(msg form)
+
+type game struct{}
+
+func (g game) On(name string, ack func(msg form)) {
+	fmt.Printf("Event: %s\n", name)
+	fmt.Println("Let's play Bomberman")
+	ack(form{
+		Header:    "application/json",
+		Body:      "encrypted_string",
+		Signature: "jwt_string",
+	})
+}
+
+func (g game) Start() *lib.Error {
+	fmt.Println("Start")
+	return nil
+}
+
+func (g game) Pause() *lib.Error {
+	fmt.Println("Pause")
+	return nil
+}
+
+func (g game) Finish() *lib.Error {
+	fmt.Println("Finish")
+	return nil
+}
+
+func main() {}
+
+// Game exported to engine
+var Game game
