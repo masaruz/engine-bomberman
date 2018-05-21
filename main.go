090210b313af3d05a28948defe773ee229e78ca6
stamp <stampz74@gmail.com>
2018-05-21 14:31:42 +0700

    Update lib

--- main.go
+++ main.go
@@ -6,50 +6,29 @@ import (
 	"github.com/masaruz/engine-lib/core"
 )
 
-type form = core.Form
-
 type ack = core.Ack
 
 type game struct{}
 
-func (g game) On(name string, ack func(msg form)) {
-	fmt.Printf("Event: %s\n", name)
-	fmt.Println("Let's play Bomberman")
-	ack(form{
-		Header:    "application/json",
-		Body:      "encrypted_string",
-		Signature: "jwt_string",
-	})
-}
-
-func (g game) Init() error {
+func (g *game) Init() error {
 	fmt.Println("Bomberman Initiated")
 	return nil
 }
 
-func (g game) Start() error {
+func (g *game) Start() error {
 	fmt.Println("Bomberman Started")
 	return nil
 }
 
-func (g game) Update(msg []byte) error {
+func (g *game) Update(msg []byte, callback ack) error {
 	fmt.Printf("Bomberman Updated With Message %s\n", string(msg))
+	callback("acknowledge")
 	return nil
 }
 
-func (g game) Pause() error {
-	fmt.Println("Bomberman Paused")
-	return nil
-}
-
-func (g game) Finish() error {
-	fmt.Println("Bomberman Finished")
-	return nil
-}
-
-func (g game) Shutdown() error {
-	fmt.Println("Bomberman Shutdown")
-	return nil
+func (g *game) GetState() ([]byte, error) {
+	fmt.Println("Bomberman Get State")
+	return []byte{}, nil
 }
 
 func main() {}
